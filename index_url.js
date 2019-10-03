0acf06663f20f098c4d14afb5bdceca8f03bf947
Vimal Pillai <vimal.pillai@vofoxsolutions.com>
2019-10-03 14:43:22 +0530

    url parameter reading done

--- index_url.js
+++ index_url.js
@@ -1,7 +1,21 @@
 var express = require('express');
 var app = express();
+// Routing Urls 
 
-app.length('/:id',function(req,res){
+app.get('/:id',function(req,res){
     res.send('The id you specified is ' + req.params.id);
 });
+
+app.get('/things/:name/:id',function(req,res){
+    res.send('id: '+req.params.id+' and name: '+ req.params.name);
+});
+
+app.get('/things/:id([0-9]{5})',function(req,res){
+    res.send('id: ' + req.params.id);
+});
+
+app.get('*',function(req,res){
+    res.send('Sorry, this is an invalid URL.');
+});
+
 app.listen(3000);
