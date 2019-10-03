c066033d530c14ab9b8a0a18d1a8cd758edb9987
Vimal Pillai <vimal.pillai@vofoxsolutions.com>
2019-10-03 14:27:13 +0530

    router done and dynamic url

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
 var express = require('express');
 var app = express();
 
-app.get('/',function(req,res){
-    res.contentType = "text/html";
-    res.send("<h1 align='center'>Hello World</h1>");
-});
-app.post('/',function(req,res){
-    res.send("You just called the post method at '/'!\n");
-});
+var things = require('./things.js');
 
-
+app.use('/',things);
 
 app.listen(3000,function(){
-    console.log("Server is running at http://192.168.10.179:3000");
+    console.log("Server is running at http://localhost:3000");
 });
 
