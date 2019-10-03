cc9f33352e85c70798bada287058e80afa1f7a4f
Vimal Pillai <vimal.pillai@vofoxsolutions.com>
2019-10-03 16:49:26 +0530

    handlebars template tags practice

--- index/index.js
+++ index/index.js
@@ -15,7 +15,26 @@ app.set("view engine", "handlebars");
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.get('/first',function(req,res){
-    res.render('index');
+    res.render('index',
+    {
+        'title':'Handle Bar',
+         languages:['A','B','C'],
+         user: {
+            admin: true
+          },
+          user2: {
+            contact: {
+              email: 'handlebar@node.com',
+              twitter: 'handle_bar'
+            },
+            address: {
+              city: 'San Francisco',
+              state: 'California'
+            },
+            name: 'Handle Bar'
+          }
+    }
+);
 });
 
 
