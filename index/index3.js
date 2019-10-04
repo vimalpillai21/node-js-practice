2d5383c99a64584d35185031f0eb2fbd60e4efa1
Vimal Pillai <vimal.pillai@vofoxsolutions.com>
2019-10-04 11:29:56 +0530

    if else string comparison in handlebars

---
+++ index/index3.js
@@ -0,0 +1,83 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var multer = require('multer');
+var exphbs = require('express-handlebars');
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/my_db', {useUnifiedTopology: true, useNewUrlParser: true});
+
+var personSchema = mongoose.Schema({
+    name: String,
+    age: Number,
+    nationality: String
+});
+
+var Person = mongoose.model("Person", personSchema);
+var upload = multer();
+var app = express();
+const port = 3000;
+
+const hbs = exphbs.create();
+
+app.engine("handlebars", exphbs({helpers:{
+        ifeq: function(a,b,options){
+            if (a === b) { return options.fn(this); }
+            return options.inverse(this);
+        },
+        ifnoteq: function(a,b,options){
+            if (a !== b) { return options.fn(this); }
+            return options.inverse(this);
+        }
+    },defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+app.use('/static', express.static('public'));
+
+// for parsing application json
+app.use(bodyParser.json());
+
+// for parsing application/xwww
+app.use(bodyParser.urlencoded({extended: true}));
+// form-urlencoded
+
+// for parsing multipart/form-data
+app.use(upload.array());
+
+app.get('/person', function (req, res) {
+    res.render('person');
+});
+
+app.post('/person', function (req, res) {
+    var personInfo = req.body;
+    if (!personInfo.name || !personInfo.age || !personInfo.nationality) {
+        res.render('show_message', {
+            message: "Sorry, youy provided wrong info", type: "error"
+        });
+    } else {
+        var newPerson = new Person({
+            name: personInfo.name,
+            age: personInfo.age,
+            nationality: personInfo.nationality
+        });
+
+        newPerson.save(function (err, Person) {
+            if (err) {
+                res.render('show_message', {
+                    message: "Database Error", type: "error"
+                });
+            } else {
+                res.render("show_message", {
+                    type: "success",
+                    message: "New Person added",
+                    person: personInfo
+                });
+            }
+        });
+    }
+});
+app.get('/people',function(req,res){
+    Person.find(function(err, response){
+        res.json(response);
+    });
+});
+app.listen(port);
